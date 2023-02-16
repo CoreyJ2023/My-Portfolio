9e1231d56915b4501418711b706c4fc0bd0e3e7c
CoreyJ2023 <125520566+CoreyJ2023@users.noreply.github.com>
2023-02-16 03:05:11 -0600

    Add files via upload

---
+++ Visual Studio Windows Forms/C#/Spring Break Travel Specials/Spring Break Travel Specials/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace Spring_Break_Travel_Specials
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCloseWindow = new System.Windows.Forms.Button();
+            this.btnPurchase = new System.Windows.Forms.Button();
+            this.lblEnjoy = new System.Windows.Forms.Label();
+            this.btnVeniceBeach = new System.Windows.Forms.Button();
+            this.btnKeyWest = new System.Windows.Forms.Button();
+            this.btnMyrtleBeach = new System.Windows.Forms.Button();
+            this.picKeyWest = new System.Windows.Forms.PictureBox();
+            this.picVeniceBeach = new System.Windows.Forms.PictureBox();
+            this.picMyrtleBeach = new System.Windows.Forms.PictureBox();
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.picKeyWest)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVeniceBeach)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMyrtleBeach)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCloseWindow
+            // 
+            this.btnCloseWindow.Location = new System.Drawing.Point(387, 583);
+            this.btnCloseWindow.Name = "btnCloseWindow";
+            this.btnCloseWindow.Size = new System.Drawing.Size(85, 23);
+            this.btnCloseWindow.TabIndex = 20;
+            this.btnCloseWindow.Text = "Close Window";
+            this.btnCloseWindow.UseVisualStyleBackColor = true;
+            this.btnCloseWindow.Click += new System.EventHandler(this.btnCloseWindow_Click);
+            // 
+            // btnPurchase
+            // 
+            this.btnPurchase.Location = new System.Drawing.Point(387, 512);
+            this.btnPurchase.Name = "btnPurchase";
+            this.btnPurchase.Size = new System.Drawing.Size(85, 23);
+            this.btnPurchase.TabIndex = 19;
+            this.btnPurchase.Text = "Purchase";
+            this.btnPurchase.UseVisualStyleBackColor = true;
+            this.btnPurchase.Click += new System.EventHandler(this.btnPurchase_Click);
+            // 
+            // lblEnjoy
+            // 
+            this.lblEnjoy.AutoSize = true;
+            this.lblEnjoy.Enabled = false;
+            this.lblEnjoy.Location = new System.Drawing.Point(396, 466);
+            this.lblEnjoy.Name = "lblEnjoy";
+            this.lblEnjoy.Size = new System.Drawing.Size(76, 13);
+            this.lblEnjoy.TabIndex = 18;
+            this.lblEnjoy.Text = "Enjoy your trip!";
+            this.lblEnjoy.Visible = false;
+            // 
+            // btnVeniceBeach
+            // 
+            this.btnVeniceBeach.Location = new System.Drawing.Point(746, 415);
+            this.btnVeniceBeach.Name = "btnVeniceBeach";
+            this.btnVeniceBeach.Size = new System.Drawing.Size(96, 23);
+            this.btnVeniceBeach.TabIndex = 17;
+            this.btnVeniceBeach.Text = "Venice Beach";
+            this.btnVeniceBeach.UseVisualStyleBackColor = true;
+            this.btnVeniceBeach.Click += new System.EventHandler(this.btnVeniceBeach_Click);
+            // 
+            // btnKeyWest
+            // 
+            this.btnKeyWest.Location = new System.Drawing.Point(387, 415);
+            this.btnKeyWest.Name = "btnKeyWest";
+            this.btnKeyWest.Size = new System.Drawing.Size(96, 23);
+            this.btnKeyWest.TabIndex = 16;
+            this.btnKeyWest.Text = "Key West";
+            this.btnKeyWest.UseVisualStyleBackColor = true;
+            this.btnKeyWest.Click += new System.EventHandler(this.btnKeyWest_Click);
+            // 
+            // btnMyrtleBeach
+            // 
+            this.btnMyrtleBeach.Location = new System.Drawing.Point(44, 415);
+            this.btnMyrtleBeach.Name = "btnMyrtleBeach";
+            this.btnMyrtleBeach.Size = new System.Drawing.Size(96, 23);
+            this.btnMyrtleBeach.TabIndex = 15;
+            this.btnMyrtleBeach.Text = "Myrtle Beach";
+            this.btnMyrtleBeach.UseVisualStyleBackColor = true;
+            this.btnMyrtleBeach.Click += new System.EventHandler(this.btnMyrtleBeach_Click);
+            // 
+            // picKeyWest
+            // 
+            this.picKeyWest.Enabled = false;
+            this.picKeyWest.Image = ((System.Drawing.Image)(resources.GetObject("picKeyWest.Image")));
+            this.picKeyWest.Location = new System.Drawing.Point(337, 141);
+            this.picKeyWest.Name = "picKeyWest";
+            this.picKeyWest.Size = new System.Drawing.Size(250, 200);
+            this.picKeyWest.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picKeyWest.TabIndex = 14;
+            this.picKeyWest.TabStop = false;
+            this.picKeyWest.Visible = false;
+            // 
+            // picVeniceBeach
+            // 
+            this.picVeniceBeach.Enabled = false;
+            this.picVeniceBeach.Image = ((System.Drawing.Image)(resources.GetObject("picVeniceBeach.Image")));
+            this.picVeniceBeach.Location = new System.Drawing.Point(669, 141);
+            this.picVeniceBeach.Name = "picVeniceBeach";
+            this.picVeniceBeach.Size = new System.Drawing.Size(250, 200);
+            this.picVeniceBeach.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picVeniceBeach.TabIndex = 13;
+            this.picVeniceBeach.TabStop = false;
+            this.picVeniceBeach.Visible = false;
+            // 
+            // picMyrtleBeach
+            // 
+            this.picMyrtleBeach.Enabled = false;
+            this.picMyrtleBeach.Image = ((System.Drawing.Image)(resources.GetObject("picMyrtleBeach.Image")));
+            this.picMyrtleBeach.Location = new System.Drawing.Point(12, 141);
+            this.picMyrtleBeach.Name = "picMyrtleBeach";
+            this.picMyrtleBeach.Size = new System.Drawing.Size(250, 200);
+            this.picMyrtleBeach.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMyrtleBeach.TabIndex = 12;
+            this.picMyrtleBeach.TabStop = false;
+            this.picMyrtleBeach.Visible = false;
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(246, -58);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(308, 25);
+            this.lblHeading.TabIndex = 11;
+            this.lblHeading.Text = "Spring Break Travel Specials";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(304, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 25);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Spring Break Travel Specials";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(974, 24);
+            this.menuStrip1.TabIndex = 22;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeToolStripMenuItem.Text = "&Close";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 702);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCloseWindow);
+            this.Controls.Add(this.btnPurchase);
+            this.Controls.Add(this.lblEnjoy);
+            this.Controls.Add(this.btnVeniceBeach);
+            this.Controls.Add(this.btnKeyWest);
+            this.Controls.Add(this.btnMyrtleBeach);
+            this.Controls.Add(this.picKeyWest);
+            this.Controls.Add(this.picVeniceBeach);
+            this.Controls.Add(this.picMyrtleBeach);
+            this.Controls.Add(this.lblHeading);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Spring Break Travel Specials";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picKeyWest)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVeniceBeach)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMyrtleBeach)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnCloseWindow;
+        internal System.Windows.Forms.Button btnPurchase;
+        internal System.Windows.Forms.Label lblEnjoy;
+        internal System.Windows.Forms.Button btnVeniceBeach;
+        internal System.Windows.Forms.Button btnKeyWest;
+        internal System.Windows.Forms.Button btnMyrtleBeach;
+        internal System.Windows.Forms.PictureBox picKeyWest;
+        internal System.Windows.Forms.PictureBox picVeniceBeach;
+        internal System.Windows.Forms.PictureBox picMyrtleBeach;
+        internal System.Windows.Forms.Label lblHeading;
+        internal System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ Visual Studio Windows Forms/C#/Spring Break Travel Specials/Spring Break Travel Specials/Form1.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Programmer:      Corey Jenkins
+ * Date:            February 17, 2021 (Updated February 8, 2023)
+ * Program Name:    Spring break Travel Specials
+ * Purpose:         To create a program in which you can select which destination you want to go.
+ *                  Once selected, it cannot be changed again. A message will appear once it has been selected.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spring_Break_Travel_Specials
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMyrtleBeach_Click(object sender, EventArgs e)
+        {
+            picMyrtleBeach.Visible = true;
+            picVeniceBeach.Visible = false;
+            picKeyWest.Visible = false;
+
+        }
+
+        private void btnKeyWest_Click(object sender, EventArgs e)
+        {
+            picKeyWest.Visible = true;
+            picVeniceBeach.Visible = false;
+            picMyrtleBeach.Visible = false;
+
+        }
+
+        private void btnVeniceBeach_Click(object sender, EventArgs e)
+        {
+            picVeniceBeach.Visible = true;
+            picKeyWest.Visible = false;
+            picMyrtleBeach.Visible = false;
+        }
+
+        private void btnPurchase_Click(object sender, EventArgs e)
+        {
+            btnKeyWest.Enabled = false;
+            btnVeniceBeach.Enabled = false;
+            btnMyrtleBeach.Enabled = false;
+            btnPurchase.Enabled = false;
+            lblEnjoy.Visible = true;
+
+        }
+
+        private void btnCloseWindow_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string message = "This program was created by Corey Jenkins.";
+            string caption = "About this program";
+            MessageBox.Show(message, caption);
+        }
+    }
+}
