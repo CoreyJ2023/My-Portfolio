f19e19314490f58ccf5d35c0f28d679e9188f551
CoreyJ2023 <125520566+CoreyJ2023@users.noreply.github.com>
2023-02-16 03:08:43 -0600

    Add files via upload

---
+++ Visual Studio Windows Forms/C#/Latte Selection/Latte Selection/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Programmer:          Corey Jenkins
+ * Date:                February 15, 2021
+ * Program Name:        Latte Selection
+ * Purpose:             To create a GUI in C# that allows you to select either Pumpkin Spice or Mocha and a button
+ *                      to select.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latte_Selection
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmLatteSelection());
+        }
+    }
+}
