f924a3dc52a0d6a942e75148ec578b816df9b5bc
CoreyJ2023 <125520566+CoreyJ2023@users.noreply.github.com>
2023-02-16 02:30:35 -0600

    Add files via upload

---
+++ Logic & Design/Visual Studio Community Edition/Decision Structures/C#/Area of Rectangles/AreaRectangles.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Programmer:      Corey Jenkins
+ * Date:            February 9, 2023
+ * Filename:        AreaRectangles.cs
+ * Purpose:         To create a program that inputs the area of two rectangles
+	                and determine if both are the same or not.
+*/
+
+using System;
+
+namespace Area_of_Rectangles
+{
+    class AreaRectangles
+    {
+        static void Main(string[] args)
+        {
+            // Function 
+            RectangleArea();
+            Console.ReadKey();
+        }
+
+        static void RectangleArea()
+        {
+            // Integers for length and width for two rectangles.
+            int length1, width1;
+            int length2, width2;
+            int rectangle1, rectangle2;
+
+            Console.WriteLine("Enter the length for the first rectangle: ");
+            length1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the width for the first rectangle: ");
+            width1 = Convert.ToInt32(Console.ReadLine());
+
+            // Area of Rectangle 1
+            rectangle1 = (length1 * width1);
+
+            // Rectangle 2
+            Console.WriteLine("Enter the length of the second rectangle: ");
+            length2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the width of the second rectangle: ");
+            width2 = Convert.ToInt32(Console.ReadLine());
+
+            // Area of Rectangle 2
+            rectangle2 = (length2 * width2);
+
+            TwoRectangles(rectangle1, rectangle2);
+        }
+
+        static void TwoRectangles(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+                Console.WriteLine("Yes! The dimensions are the same.");
+            }
+            else
+            {
+                Console.WriteLine("No! The dimensions are not the same.");
+            }
+        }
+    }
+}
---
+++ Logic & Design/Visual Studio Community Edition/Decision Structures/C#/Roman Numerals/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Programmer:          Corey Jenkins
+ * Date:                February 9, 2023
+ * Filename:            RomanNumerals.cs
+ * Purpose:             To create a program that gives an error if a Roman Numeral is used outside I through X
+*/
+
+using System;
+
+namespace Roman_Numerals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RomanNumeral();
+            Console.ReadKey();
+        }
+
+        static void RomanNumeral()
+        {
+            int numeral;
+
+            Console.WriteLine("Enter a number between 1 and 10.");
+            numeral = Convert.ToInt32(Console.ReadLine());
+
+            switch (numeral)
+            {
+                case 1:
+                    Console.WriteLine("I");
+                    break;
+
+                case 2:
+                    Console.WriteLine("II");
+                    break;
+
+                case 3:
+                    Console.WriteLine("III");
+                    break;
+
+                case 4:
+                    Console.WriteLine("IV");
+                    break;
+
+                case 5:
+                    Console.WriteLine("V");
+                    break;
+
+                case 6:
+                    Console.WriteLine("VI");
+                    break;
+
+                case 7:
+                    Console.WriteLine("VII");
+                    break;
+
+                case 8:
+                    Console.WriteLine("VIII");
+                    break;
+
+                case 9:
+                    Console.WriteLine("XI");
+                    break;
+
+                case 10:
+                    Console.WriteLine("X");
+                    break;
+
+                default:
+                    Console.WriteLine("An error has occurred.");
+                    break;
+            }
+        }
+    }
+}
