c58d3b9700af218afdd1c6b9f55cd0e049351e23
CoreyJ2023 <125520566+CoreyJ2023@users.noreply.github.com>
2023-08-16 10:56:49 -0500

    Add files via upload

---
+++ Logic & Design/Visual Studio Community Edition/Functions/C#/areaRectangleFunction/areaRectangleFunction.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System;
+/*
+ * Programmer:      Corey Jenkins
+ * Date:            August 15, 2023
+ * Filename:        areaRectangleFunction.cs
+ * Purpose:         A program in which returns the area of a rectangle.
+*/
+namespace areaRectangleFunction
+{
+    class areaRectangleFunction
+    {
+        static void Main(string[] args)
+        {
+            int rectangleLength, rectangleWidth, areaFunction;
+            Console.WriteLine("Enter the length: ");
+            rectangleLength = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the width: ");
+            rectangleWidth = Convert.ToInt32(Console.ReadLine());
+
+            areaFunction = area(rectangleLength, rectangleWidth);
+            Console.WriteLine(areaFunction);
+            Console.ReadKey(); // Pause the program.
+        }
+
+        static int area(int num1, int num2)
+        {
+            int result;
+            result = num1 * num2;
+            return result;
+        }
+    }
+}
---
+++ Logic & Design/Visual Studio Community Edition/Functions/C#/feetToInchesFunction/feettoInchesFunction.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Programmer:      Corey Jenkins
+ * Date:            August 15, 2023
+ * Filename:        feetToInchesFunction.cs
+ * Purpose:         To input feet and with a function to return it to inches.
+ */
+
+using System;
+
+namespace feetToInchesFunction
+{
+    class feettoInchesFunction
+    {
+        static void Main(string[] args)
+        {
+            double feet, convertToInches;
+            Console.WriteLine("Enter the amount of feet (distance): ");
+            feet = Convert.ToDouble(Console.ReadLine());
+
+            convertToInches = feetToInches(feet);
+            Console.WriteLine(convertToInches);
+            Console.ReadKey();
+        }
+
+        static double feetToInches(double num1)
+        {
+            double result;
+            result = num1 / 12; // 1 ft = 12 inches. Divide.
+            return result;
+        }
+    }
+}
